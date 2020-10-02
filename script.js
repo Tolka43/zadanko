593618ac3cfb41d4a9dec95d3a63e9420054f83f
Ola Tomczyk <ola443@op.pl>
2020-10-02 18:40:43 +0200

    changes

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-const input = document.querySelector('input');
+let users;
+
+const input = document.querySelector("input");
 
 function fetchUsers(count) {
   fetch(`https://randomuser.me/api?results=${count}`)
     .then((res) => res.json())
     .then((res) => {
       createUsers(res.results);
+      users = res.results;
     });
 }
 
-const downloadButton = document.querySelector('.download-button');
-downloadButton.addEventListener('click', () => {
+const downloadButton = document.querySelector(".download-button");
+downloadButton.addEventListener("click", () => {
   fetchUsers(input.value);
 });
 
-const mainDiv = document.querySelector('.main');
+const mainDiv = document.querySelector(".main");
 
 function createUser(user) {
-  const userContainer = document.createElement('div');
+  const userContainer = document.createElement("div");
   userContainer.innerText = `Jestem ${user.name.first}, mam lat ${user.dob.age}.`;
   mainDiv.appendChild(userContainer);
 }
@@ -24,3 +27,35 @@ function createUser(user) {
 function createUsers(users) {
   users.forEach(createUser);
 }
+
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+
+function clear() {
+  mainDiv.innerHTML = "";
+}
+
+function radioListener(event) {
+  if (!users) {
+    return;
+  }
+  if (event.target.value === "all") {
+    clear();
+    createUsers(users);
+    return;
+  }
+  if (event.target.value === "male") {
+    clear();
+    const male = users.filter((user) => user.gender === "male");
+    createUsers(male);
+    return;
+  }
+  if (event.target.value === "female") {
+    clear();
+    const female = users.filter((user) => user.gender === "female");
+    createUsers(female);
+  }
+}
+
+radioButtons.forEach((input) => {
+  input.addEventListener("change", radioListener);
+});
